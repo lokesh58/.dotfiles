013d0ba23cadf4937794c80447bcf9fddd195924
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-04-27 22:02:44 +0530

    Add basic editor options

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,36 @@
+-- Setup editor
+vim.opt.confirm = true
+vim.opt.fixendofline = true
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+vim.opt.undofile = false
+vim.opt.updatetime = 250
+
+-- Setup basic UI
+vim.opt.breakindent = true
+vim.opt.cursorline = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 5
+vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wrap = true
+
+-- Setup indentation
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+-- Setup search
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.smartcase = true
