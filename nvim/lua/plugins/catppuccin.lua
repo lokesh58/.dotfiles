04bbf04388df1d19824018478b21fcd460a9d7ff
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-04-28 08:44:29 +0530

    Add Lazy and Catppuccin

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+        })
+        -- load the colorscheme
+        vim.cmd.colorscheme("catppuccin")
+    end,
+}
