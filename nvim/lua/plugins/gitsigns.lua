620c37148956e79de15aead867c42eca760774df
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-04-30 17:45:12 +0530

    add few more git hunk keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -14,7 +14,9 @@ return {
         { "<leader>gB", "<cmd>Gitsigns blame<CR>", desc = "Git blame file" },
         { "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>", desc = "Preview Git hunk in a popup" },
         { "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", desc = "Reset current Git hunk" },
+        { "[H", "<cmd>Gitsigns nav_hunk last<CR>", desc = "Goto last Git hunk" },
         { "[h", "<cmd>Gitsigns nav_hunk prev<CR>", desc = "Goto previous Git hunk" },
         { "]h", "<cmd>Gitsigns nav_hunk next<CR>", desc = "Goto next Git hunk" },
+        { "]H", "<cmd>Gitsigns nav_hunk first<CR>", desc = "Goto first Git hunk" },
     },
 }
