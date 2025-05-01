4d9ae0e8a1fe6771c72cb66dc21a113d0c9e089e
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-05-01 08:16:59 +0530

    reduce timeout for yank highlight

--- nvim/lua/core/autocommands.lua
+++ nvim/lua/core/autocommands.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
     group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
     callback = function()
-        vim.highlight.on_yank({ timeout = 500 })
+        vim.highlight.on_yank({ timeout = 300 })
     end,
 })
