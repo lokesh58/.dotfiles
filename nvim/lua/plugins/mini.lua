890db9e2534b6b226fd419fa10268b3e6fab2f75
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-05-08 08:08:35 +0530

    update mini surround keybinds to that of lazyvim

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -48,13 +48,13 @@ return {
         "echasnovski/mini.surround",
         opts = {
             mappings = {
-                add = "ys", -- Add surrounding in Normal and Visual modes
-                delete = "ds", -- Delete surrounding
-                find = "", -- Find surrounding (to the right)
-                find_left = "", -- Find surrounding (to the left)
-                highlight = "", -- Highlight surrounding
-                replace = "cs", -- Replace surrounding
-                update_n_lines = "", -- Update `n_lines`
+                add = "gsa", -- Add surrounding in Normal and Visual modes
+                delete = "gsd", -- Delete surrounding
+                find = "gsf", -- Find surrounding (to the right)
+                find_left = "gsF", -- Find surrounding (to the left)
+                highlight = "gsh", -- Highlight surrounding
+                replace = "gsr", -- Replace surrounding
+                update_n_lines = "gsn", -- Update `n_lines`
 
                 suffix_last = "l", -- Suffix to search with "prev" method
                 suffix_next = "n", -- Suffix to search with "next" method
