b539e3212213cc93d68eb497aa9b7c2095fe4c25
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-07-13 17:58:06 +0530

    fix scrollbar

--- nvim/lua/plugins/winbar-statusline.lua
+++ nvim/lua/plugins/winbar-statusline.lua
@@ -268,7 +268,7 @@ return {
                 provider = function(self)
                     local curr_line = vim.api.nvim_win_get_cursor(0)[1]
                     local lines = vim.api.nvim_buf_line_count(0)
-                    local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
+                    local i = math.ceil(curr_line * #self.sbar / lines)
                     return string.rep(self.sbar[i], 2)
                 end,
                 hl = { fg = "bright_bg", bg = "blue" },
@@ -652,8 +652,6 @@ return {
                     Navic,
                 },
             }
-
-            -- Statuscol !!
 
             require("heirline").setup({
                 statusline = StatusLines,
