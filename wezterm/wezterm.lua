41cd22a241db9ee7938c54bb785fd80d29a63718
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-10-23 12:37:36 +0530

    update to use pwsh in windows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,4 +7,8 @@ config.color_scheme = "Catppuccin Mocha"
 config.font_size = 12
 config.font = wezterm.font("MesloLGM Nerd Font")
 
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "pwsh.exe" }
+end
+
 return config
